65fdf12840a04453c1ecc3544ab97fb859365716
Brian Richardson <brichson@ad.unc.edu>
2026-08-24 09:17:46 -0400

    update crown

--- R/nonpar_est.R
+++ R/nonpar_est.R
@@ -1,26 +1,26 @@
 ## nonparametric nuisance estimation wrapper funtion
 nonpar_est <- function(x, y, method, arguments = NULL, wts = NULL) {
-  
+
   mod <- NULL
-  
+
   if (method == "SuperLearner") {
-    
+
     mod <- SuperLearner::SuperLearner(
-      Y = y,
+      Y = as.numeric(as.character(y)),
       X = x,
       family = binomial(),
       obsWeights = wts,
       SL.library = arguments$SL.library,
       cvControl = arguments$cvControl)
-    
+
   } else if (method == "xgboost") {
-    
+
     mod <- xgboost::xgboost(
       x = x,
       y = y,
       objective = "binary:logistic",
       weights = wts)
-    
+
   } else {
     print("unrecognized method")
   }
@@ -30,26 +30,26 @@ nonpar_est <- function(x, y, method, arguments = NULL, wts = NULL) {
 
 ## nonparametric nuisance prediction wrapper function
 nonpar_pred <- function(mod, newdata, method) {
-  
+
   pred <- NULL
-  
+
   if (method == "SuperLearner") {
-    
+
     pred <- predict(
       mod,
       newdata = newdata,
       onlySL = T)$pred
-    
+
   } else if (method == "xgboost") {
-    
+
     pred <- predict(
       mod,
       newdata = newdata)
-    
+
   } else {
     print("unrecognized method")
   }
-  
+
   return(pred)
-  
+
 }
